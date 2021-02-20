f0fae2059d229df4eda22afc629daff693923e8f
Z <asterixandisaac@gmail.com>
2021-02-19 20:51:39 -1000

    change

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -59,7 +59,7 @@ class Calculator {
     if int a = 16 then this method returns: 10000
      */
     String intToBinaryNumber(int n){
-        return Integer.toBinaryString(n);
+        return Integer.toBinaryString(n) ;
     }
 
     /*
